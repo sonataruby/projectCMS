c74a3aae04f7132ede0e7daa685077a3dcb5a8a0
Roller Davic <sonataruby@gmail.com>
2022-03-29 15:37:01 +0700

    First Update

---
+++ app/Controllers/Signal.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Controllers;
+use App\Models\TraderModel;
+class Signal extends BaseController
+{
+	private $query;
+	public function __construct(){
+		$this->query = new TraderModel;
+	}
+    public function index()
+    {
+        // check if already logged in.
+		if (!logged_in())
+		{
+			return redirect()->back();
+		}
+		$search = $this->request->getGet('fillter');
+
+        $data = $this->query->getSignal($search);
+        $finish = $this->query->getSignalFinish();
+		return view('pages/signal',["data" => $data, "finish" => $finish, "header" => ["title" => "Smart Signal"]]);
+	}
+
+	public function attemptProfile(){
+
+	}
+
+	public function api($type=""){
+		$data  = json_decode($this->request->getGet('query'));
+		
+		if($type == "create"){
+			$arv = [
+				
+				"symbol" => $data->symbol, 
+				"type" => $data->type, 
+				"open" => $data->open,
+				"open_2" => $data->dca1, 
+				"open_3" => $data->dca2, 
+				"sl" => $data->sl, 
+				"tp" => $data->tp,
+				"tp_2" => $data->tp2, 
+				"tp_3" => $data->tp3,  
+				"message_id" => $data->telegram,
+				"message_id_group" => $data->relymsg,
+				"timefream" => $data->tf, 
+				"chart" => ""
+			];
+
+			$this->query->createOrder($arv);
+			$client = \Config\Services::curlrequest();
+
+			$client->request('post', 'http://127.0.0.1:7000/signal', ["json" => $arv]);
+			echo json_encode(["status" => "ok"]);
+		}
+
+
+		if($type == "finish"){
+			$arv = [
+				"target" => $data->target,
+				"pip" => $data->pip,
+				"close" => $data->close_at,
+				"close_type" => strtolower($data->type),
+				"usd" => $data->usd,
+				"message_id" => $data->telegram
+			];
+			$this->query->finishOrder($arv);
+			echo json_encode(["status" => "ok"]);
+		}
+	}
+}
---
+++ app/Models/TraderModel.php
@@ -0,0 +1,57 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+
+class TraderModel extends Model
+{
+	protected $table = 'trader_signal';
+    protected $primaryKey = 'id';
+	public function getSignal($s=""){
+		$query = $this->db->table('trader_signal');
+		if($s != ""){
+			$query->like("symbol",$s);
+		}
+		$query->orderBy("id","DESC");
+		$query = $query->get(10);
+		return $query->getResult();
+
+	}
+
+	public function getSignalFinish(){
+		$query = $this->db->table('trader_signal_finish')->get(15);
+		return $query->getResult();
+
+	}
+
+	public function createOrder($obj){
+		//print_r($obj);
+		$this->db->table('trader_signal')->insert($obj);
+	}
+
+
+	public function finishOrder($obj){
+		$info = $this->db->table('trader_signal')->where(["message_id" => $obj->message_id])->get(1);
+		$action = $obj->close_type;
+		if($action == "sl" || $obj->target > 2){
+			$this->db->table('trader_signal')->where(["message_id" => $obj->message_id])->delete();//Remove Complete Order
+		}
+		$arv = [
+				"signals_id" => $info->id,
+				"type" => $info->type,
+				"symbol" => $info->symbol,
+				"open" => $info->open,
+				"opentime" => $info->opentime,
+				"sl" => $info->sl,
+				"close_at" => $info->close,
+				"close_time" => date("m-d-Y h:i:s"),
+				"profit_pip" => $obj->pip,
+				"profit_usd" => $obj->usd,
+				"close_type" => ($obj->close_type == "sl" || $obj->close_type == "tp" ?  strtoupper($obj->close_type) : "Close"),
+				"message_id" => $info->message_id,
+				"is_access" => $obj->target < 2 ? "Free" : "Vip"
+			];
+		$this->db->table('trader_signal_finish')->insert($arv);
+	}
+}
+
