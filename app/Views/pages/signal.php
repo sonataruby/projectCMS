72e56aa45a88188c8798c4f3c2d462d9c427d912
Roller Davic <sonataruby@gmail.com>
2022-03-31 09:04:26 +0700

    Update signal.php

--- app/Views/pages/signal.php
+++ app/Views/pages/signal.php
@@ -309,7 +309,12 @@
                       <td><?php echo $item->open;?></td>
                       <td><?php echo $item->sl;?></td>
                       <td><?php echo $item->tp;?></td>
-                      <td class="text-end"><?php echo $item->status_pips;?> pip | <?php echo $item->status_usd;?>$ <button class="btn btn-icon-only btn-rounded btn-outline-primary mb-0 me-3 btn-sm  align-items-center justify-content-center"><i class="fas fa-arrow-right"></i></button></td>
+                      <td class="text-end">
+                        <div class="d-flex flex-column">
+                          <h6 class="mb-1 text-dark text-sm"><?php echo $item->status_usd;?>$</h6>
+                          <span class="text-xs"><?php echo $item->status_pips;?> pip</span>
+                        </div>
+                        <button class="btn btn-icon-only btn-rounded btn-outline-primary mb-0 me-3 btn-sm  align-items-center justify-content-center"><i class="fas fa-arrow-right"></i></button></td>
                     </tr>
                     <?php } 
                     
